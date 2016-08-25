0ceedec13c2491be01f1401f2f4809083ac29f07
ctliu3 <lcndn3@gmail.com>
2016-08-25 18:44:40 +0800

    Add basic web UI.

--- server/server.go
+++ server/server.go
@@ -1,25 +1,84 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
+	"html/template"
+	// "io"
 	"log"
 	"net/http"
+	// "time"
+
+	"github.com/gorilla/mux"
 )
 
-type Workers struct {
+const (
+	STATIC_URL = "/static/"
+	STATIC_DIR = "static/"
+)
+
+type WorkerState int
+
+const (
+	RUNNING WorkerState = iota
+	STOP
+	LOST
+)
+
+type Worker struct {
+	WID          int32
+	IP           string
+	NumProcessed int32
+	Statue       WorkerState
+	LastUpdated  time
 }
+
+type Context struct {
+	Title  string
+	Static string
+}
+
+var (
+	Workers       []Worker
+	CPUUtilThresh float32
+)
 
 func main() {
 	router := mux.NewRouter()
+
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(STATIC_DIR))))
+
+	router.HandleFunc("/", monitorHandler).Methods("GET")
+	// Display the status of each worker on web.
+	router.HandleFunc("/monitor", monitorHandler).Methods("GET")
 	router.HandleFunc("/info", info).Methods("GET")
-	router.HandleFunc("/wokers", getWorkers).Methods("GET")
-	router.HandleFunc("/woker", registerWorker).Methods("POST")
-	router.HandleFunc("/woker", upateWorker).Methods("PUT")
+	router.HandleFunc("/workers", getWorkers).Methods("GET")
+
+	router.HandleFunc("/health", registerWorker).Methods("POST")
+	router.HandleFunc("/health", updateWorker).Methods("PUT")
 
 	err := http.ListenAndServe(":8089", router)
 	if err != nil {
 		log.Fatal("Start server err: ", err)
 	}
+}
+
+func render(w http.ResponseWriter, tpl string, context Context) {
+	context.Static = STATIC_URL
+	tpls := []string{"templates/base.html", fmt.Sprintf("templates/%s.html", tpl)}
+	t, err := template.ParseFiles(tpls...)
+	if err != nil {
+		log.Fatal("Template parse err: ", err)
+		return
+	}
+	err = t.Execute(w, context)
+	if err != nil {
+		log.Fatal("Template execute err: ", err)
+	}
+}
+
+func monitorHandler(w http.ResponseWriter, r *http.Request) {
+	context := Context{Title: "monitor"}
+	render(w, "monitor", context)
 }
 
 func info(w http.ResponseWriter, r *http.Request) {
@@ -32,5 +91,5 @@ func getWorkers(w http.ResponseWriter, r *http.Request) {
 func registerWorker(w http.ResponseWriter, r *http.Request) {
 }
 
-func upateWorker(w http.ResponseWriter, r *http.Request) {
+func updateWorker(w http.ResponseWriter, r *http.Request) {
 }
